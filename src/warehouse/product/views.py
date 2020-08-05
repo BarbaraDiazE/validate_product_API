d0630df0e39355b6ba42b75b9208bf6da21a574d
Barbara <debi_1223@hotmail.com>
2020-08-05 18:59:17 -0500

    add product app to install apps, create validation_viwes on file viwes.py and associate validation_viwes with an url

--- src/warehouse/product/views.py
+++ src/warehouse/product/views.py
@@ -1,3 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def validation_view(request):
+    product = {"id": "A_120", "name": "Leche",
+               "value": 10.00, "discount": 0.10, "stock": 5}
+    return JsonResponse(product)
