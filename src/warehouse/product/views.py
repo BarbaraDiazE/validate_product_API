c7e2756dbdfab52ad58a341899965f385bff9dd9
Barbara <debi_1223@hotmail.com>
2020-08-05 20:34:28 -0500

    add request method validation

--- src/warehouse/product/views.py
+++ src/warehouse/product/views.py
@@ -1,20 +1,23 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Product
 
 
+@csrf_exempt
 def product_view(request):
-    result = {
-        "products": [
-            {
-                "id": p.id,
-                "name": p.name,
-                "value": p.value,
-                "discount": p.discount,
-                "stock": p.stock,
-            }
-            for p in Product.objects.all()
-        ]
-    }
-    return JsonResponse(result)
+    if request.method == "GET":
+        result = {
+            "products": [
+                {
+                    "id": p.id,
+                    "name": p.name,
+                    "value": p.value,
+                    "discount": p.discount,
+                    "stock": p.stock,
+                }
+                for p in Product.objects.all()
+            ]
+        }
+        return JsonResponse(result, status=200)
+    return HttpResponse(status=405)
