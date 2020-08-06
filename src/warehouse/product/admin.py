84ad78f541b46ab166f23835b5d4eb5e3f124dbe
Barbara <debi_1223@hotmail.com>
2020-08-05 20:13:44 -0500

    add model and admin for Product

--- src/warehouse/product/admin.py
+++ src/warehouse/product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from product.models import Product
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name", "value", "discount", "stock")
--- src/warehouse/product/models.py
+++ src/warehouse/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.CharField(primary_key=True, max_length=55)
+    name = models.CharField(max_length=100)
+    value = models.FloatField()
+    discount = models.FloatField()
+    stock = models.IntegerField()
